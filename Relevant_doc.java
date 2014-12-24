bab1c066ea52102599295f4f85d6341d9ed041c9
xiuwensun <sunxw10@gmail.com>
2014-12-24 17:15:41 -0500

    Create Relevant_doc.java

---
+++ Relevant_doc.java
@@ -0,0 +1,10 @@
+package sun;
+
+public class Relevant_doc {
+	public Document doc;
+	public double score;
+	public Relevant_doc(Document d, double score){
+		this.doc=d;
+		this.score=score;
+	}
+}
